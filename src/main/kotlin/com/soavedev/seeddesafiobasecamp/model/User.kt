8e929ea8a7f974aa39c523d17f9dc9dfcf694630
Arthur Soave <atrpaula@gmail.com>
2024-01-13 11:10:21 -0300

    [feat] create user entity

---
+++ src/main/kotlin/com/soavedev/seeddesafiobasecamp/model/User.kt
@@ -0,0 +1,16 @@
+package com.soavedev.seeddesafiobasecamp.model
+
+import com.soavedev.seeddesafiobasecamp.model.enums.UserRoles
+import com.soavedev.seeddesafiobasecamp.model.enums.UserStatus
+import java.util.UUID
+
+data class User(
+        var id: UUID,
+        var name: String,
+        var emailAddress: String,
+        var role: UserRoles,
+        var status: UserStatus,
+        var location: String,
+        var shortBio: String,
+        var profilePictureUrl: String
+)
