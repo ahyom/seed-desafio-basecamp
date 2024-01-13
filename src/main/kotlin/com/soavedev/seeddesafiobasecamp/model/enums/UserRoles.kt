69da6e377e52b8bf6e28f353c9866636e2b33815
Arthur Soave <atrpaula@gmail.com>
2024-01-13 11:06:00 -0300

    [feat] user enums

---
+++ src/main/kotlin/com/soavedev/seeddesafiobasecamp/model/enums/UserRoles.kt
@@ -0,0 +1,7 @@
+package com.soavedev.seeddesafiobasecamp.model.enums
+
+enum class UserRoles {
+    ADMIN,
+    WRITER,
+    READER
+}
---
+++ src/main/kotlin/com/soavedev/seeddesafiobasecamp/model/enums/UserStatus.kt
@@ -0,0 +1,6 @@
+package com.soavedev.seeddesafiobasecamp.model.enums
+
+enum class UserStatus {
+    ACTIVE,
+    INACTIVE
+}
