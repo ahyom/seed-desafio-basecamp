490bb8548abd98c3efde9ed40fe4e29f8e3e4c25
Arthur Soave <atrpaula@gmail.com>
2024-01-13 11:29:23 -0300

    [feat] define tb_user

--- src/main/kotlin/com/soavedev/seeddesafiobasecamp/model/entity/User.kt
+++ src/main/kotlin/com/soavedev/seeddesafiobasecamp/model/entity/User.kt
@@ -2,15 +2,33 @@ package com.soavedev.seeddesafiobasecamp.model.entity
 
 import com.soavedev.seeddesafiobasecamp.model.enums.UserRoles
 import com.soavedev.seeddesafiobasecamp.model.enums.UserStatus
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
 import java.util.UUID
 
+@Entity
+@Table(name = "tb_user")
 data class User(
+        @Id
         var id: UUID,
+
+        @Column(nullable = false)
         var name: String,
+
+        @Column(nullable = false, unique = true)
         var emailAddress: String,
-        var role: UserRoles,
-        var status: UserStatus,
+
+        @Column(nullable = false)
+        var role: String,
+
+        @Column(nullable = false)
+        var status: String,
+
         var location: String,
+
         var shortBio: String,
+
         var profilePictureUrl: String
 )
