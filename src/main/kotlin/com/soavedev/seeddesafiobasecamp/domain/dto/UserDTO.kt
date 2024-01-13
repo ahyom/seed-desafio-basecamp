9967fd53c25cf21d7a663a6b1a242108a9deca85
Arthur Soave <atrpaula@gmail.com>
2024-01-13 17:38:15 -0300

    [feat] set validations and test coverage

--- src/main/kotlin/com/soavedev/seeddesafiobasecamp/domain/dto/UserDTO.kt
+++ src/main/kotlin/com/soavedev/seeddesafiobasecamp/domain/dto/UserDTO.kt
@@ -2,15 +2,21 @@ package com.soavedev.seeddesafiobasecamp.domain.dto
 
 import com.soavedev.seeddesafiobasecamp.domain.enums.UserRoles
 import com.soavedev.seeddesafiobasecamp.domain.enums.UserStatus
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
 import java.util.UUID
 
 data class UserDTO(
 
         var id: UUID?,
 
-
+        @field:NotEmpty(message = "name must be informed")
+        @field:Size(max = 50, message = "name must have max of 50 characters")
         var name: String,
 
+        @field:Email(message = "email must be valid")
+        @field:NotEmpty(message = "email must be informed")
         var emailAddress: String,
 
         var role: UserRoles,
