6cb45a1f3a81e24d13252afd63528b720309ab10
Arthur Soave <atrpaula@gmail.com>
2024-01-25 10:55:34 -0300

    [feat] implement buckets classes

---
+++ src/main/kotlin/com/soavedev/seeddesafiobasecamp/domain/dto/BucketDTO.kt
@@ -0,0 +1,35 @@
+package com.soavedev.seeddesafiobasecamp.domain.dto
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import jakarta.validation.Validation
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Size
+import java.time.LocalDateTime
+import java.util.UUID
+
+data class BucketDTO (
+        var id: UUID?,
+
+        @field:NotEmpty(message = "name must be informed")
+        @field:Size(max = 100, message = "name must have max of 100 characters")
+        var name: String,
+
+        @field:Size(max = 400, message = "name must have max of 400 characters")
+        var description: String,
+
+        var createdBy: String,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        var startDate: LocalDateTime?,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+        var endDate: LocalDateTime?,
+
+        var taskIds: List<String> = emptyList()
+) {
+
+        fun validate(): List<String> {
+                val validator = Validation.buildDefaultValidatorFactory().validator
+                return validator.validate(this).map { it.message }
+        }
+}
